a00cacd4c7398cdbd5dee2a5c844e1e39447714a
Unknown <cynthiacyndewi@gmail.com>
2018-07-19 10:11:42 +0700

    media crud view

---
+++ resources/views/media/index.blade.php
@@ -0,0 +1,97 @@
+@extends('master.app')
+@section('title') Data Media @endsection
+@section('css')
+<!-- DataTables -->
+  <link rel="stylesheet" href="AdminLTE/plugins/datatables/dataTables.bootstrap4.css">
+@endsection
+@section('content')
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Media</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="/">Home</a></li>
+              <li class="breadcrumb-item active">Media</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Data Media
+            <div style="float: right;">
+              <a href="media/tambah"><button type="button" class="btn btn-block btn-primary">+ Tambah</button></a>
+            </div>
+          </h3>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+          <table id="tabel-media" class="table table-bordered table-striped">
+            <thead style="text-align: center">
+              <tr>
+                <th width="5%">No</th>
+                <th>Nama</th>
+                <th>Kategori</th>
+                <th width="5%"></th>
+                <th width="5%"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Giat</td>
+                <td>Publik</td>
+                <td><a href="/media/edit"><button type="button" class="btn btn-block btn-warning btn-sm"><span class="fa fa-edit"></span></button></a></td>
+                <td><a href="#"><button type="button" class="btn btn-block btn-danger btn-sm"><span class="fa fa-trash"></span></button></a></td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Giat</td>
+                <td>Inhouse</td>
+                <td><a href="/media/edit"><button type="button" class="btn btn-block btn-warning btn-sm"><span class="fa fa-edit"></span></button></a></td>
+                <td><a href="#"><button type="button" class="btn btn-block btn-danger btn-sm"><span class="fa fa-trash"></span></button></a></td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Juara</td>
+                <td>Inhouse</td>
+                <td><a href="/media/edit"><button type="button" class="btn btn-block btn-warning btn-sm"><span class="fa fa-edit"></span></button></a></td>
+                <td><a href="#"><button type="button" class="btn btn-block btn-danger btn-sm"><span class="fa fa-trash"></span></button></a></td>
+              </tr>
+          </table>
+        </div>
+        <!-- /.card-body -->
+      </div>
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+</div>
+@endsection
+@section('script')
+<!-- jQuery -->
+<script src="AdminLTE/plugins/jquery/jquery.min.js"></script>
+<!-- DataTables -->
+<script src="AdminLTE/plugins/datatables/jquery.dataTables.js"></script>
+<script src="AdminLTE/plugins/datatables/dataTables.bootstrap4.js"></script>
+<script>
+  $(function () {
+    $("#tabel-media").DataTable();
+  });
+</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+
+@endsection
+
---
+++ resources/views/media/tambah.blade.php
@@ -0,0 +1,76 @@
+@extends('master.app')
+@section('title') Tambah Data Media @endsection
+@section('css')
+<!-- Select2 -->
+<link rel="stylesheet" href="/AdminLTE/plugins/select2/select2.min.css">
+@endsection
+@section('content')
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Media</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="/">Home</a></li>
+              <li class="breadcrumb-item"><a href="/media">Media</a></li>
+              <li class="breadcrumb-item active">Tambah Media</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Tambah Data Media</h3>
+        </div>
+        <!-- /.card-header -->
+        <!-- form start -->
+        <form role="form">
+          <div class="card-body">
+            <div class="form-group">
+              <label for="nama_media">Nama</label>
+              <input name="nama_media" type="text" class="form-control" id="nama_media" placeholder="Masukkan Nama Media">
+            </div>
+            <div class="form-group">
+              <label for="divisi_pegawai">Kategori Media</label>
+              <select class="form-control select2" style="width: 100%;">
+                <option selected="selected">Inhouse</option>
+                <option>Publik</option>
+              </select>
+            </div>
+          </div>
+          <!-- /.card-body -->
+
+          <div class="card-footer">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+@endsection
+@section('script')
+<!-- jQuery -->
+<script src="/AdminLTE/plugins/jquery/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+<!-- Select2 -->
+<script src="/AdminLTE/plugins/select2/select2.full.min.js"></script>
+<script>
+  $(function () {
+    //Initialize Select2 Elements
+    $('.select2').select2()
+  })
+</script>
+@endsection
+
